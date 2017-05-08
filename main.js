f74822752cfd6d97742ef47532ded43c1a893247
wallytreats <nicklawrenceemail@gmail.com>
2017-05-08 13:19:14 -0600

    final commit

--- main.js
+++ main.js
@@ -12,50 +12,94 @@ Run the command 'mocha' to test. Tests check for expected output and absence of 
 
 module.exports = {
 
+  // sum: (arr, base) => {
+  //   let sum = base;
+  //   for (var i = 0; i < arr.length; i++){
+  //     sum += arr[i];
+  //   }
+  //   return sum;
+  // },
+
+
   sum: (arr, base) => {
-    let sum = base;
-    for (var i = 0; i < arr.length; i++){
-      sum += arr[i];
-    }
-    return sum;
+    var results = arr.reduce(function (acc, curr){
+      return acc + curr;
+    }, base);
+    return results;
   },
+
+
+  // someObjsContainProp: (arr, prop) => {
+  //   for(var i = 0; i < arr.length; i++){
+  //     if(arr[i].hasOwnProperty(prop)){
+  //       return true;
+  //     }
+  //   }
+  //   return false;
+  // },
 
   someObjsContainProp: (arr, prop) => {
-    for(var i = 0; i < arr.length; i++){
-      if(arr[i].hasOwnProperty(prop)){
-        return true;
-      }
-    }
-    return false;
+    return arr.some(function (element) {
+      return element.hasOwnProperty(prop);
+    });
   },
+//
+//
+//   // convertNameArrayToObject: (arr) => {
+//   //   let nameObj = [];
+//   //   for(var i = 0; i < arr.length; i++){
+//   //     let obj = {};
+//   //     obj.first = arr[i][0];
+//   //     obj.last = arr[i][1];
+//   //     nameObj.push(obj);
+//   //   }
+//   //   return nameObj;
+//   // },
 
   convertNameArrayToObject: (arr) => {
-    let nameObj = [];
-    for(var i = 0; i < arr.length; i++){
-      let obj = {};
-      obj.first = arr[i][0];
-      obj.last = arr[i][1];
-      nameObj.push(obj);
-    }
-    return nameObj;
+    return arr.map(function(element) {
+      var newObj = {};
+      newObj.first = element[0];
+      newObj.last = element[1];
+      return newObj;
+  });
   },
 
-  objContainsProp: (arr, prop) => {
-    for (var i = 0; i < arr.length; i++){
-      if(!arr[i].hasOwnProperty(prop)){
-        return false;
-      }
-    }
-    return true;
-  },
+//   // objContainsProp: (arr, prop) => {
+//   //   for (var i = 0; i < arr.length; i++){
+//   //     if(!arr[i].hasOwnProperty(prop)){
+//   //       return false;
+//   //     }
+//   //   }
+//   //   return true;
+//   // },
 
-  stringMatch: (arr, str) => {
-    let matches = [];
-    for(var i = 0; i < arr.length; i++){
-      if (arr[i].includes(str)){
-        matches.push(arr[i]);
+objContainsProp: (arr, prop) => {
+  return arr.every(function (element) {
+    return element.hasOwnProperty(prop);
+  });
+},
+
+
+// //   stringMatch: (arr, str) => {
+// //     let matches = [];
+// //     for(var i = 0; i < arr.length; i++){
+// //       if (arr[i].includes(str)){
+// //         matches.push(arr[i]);
+// //       }
+// //     }
+// //     return matches;
+// //   },
+// // };
+
+  stringMatch: (arr, letter) => {
+    return arr.filter(function (element, index) {
+      if (element.includes(letter)){
+        return element;
       }
-    }
-    return matches;
-  },
+    return;
+  });
+}
+
+
 };
